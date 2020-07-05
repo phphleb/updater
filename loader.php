c48e359c80dad1f1d93dc48a0ba1872c49b2f58a
Foma Tuturov <fomiash@yandex.ru>
2020-07-05 14:15:15 +0300

    upd data path

--- loader.php
+++ loader.php
@@ -1,8 +1,10 @@
 <?php
 
+include_once __DIR__ . "/classes/Data.php";
 include_once __DIR__ . "/classes/SearchDirections.php";
 include_once __DIR__ . "/classes/LogInterface.php";
 include_once __DIR__ . "/classes/OutputLog.php";
 include_once __DIR__ . "/classes/AddLogin.php";
 include_once __DIR__ . "/classes/RemoveLogin.php";
+
 
