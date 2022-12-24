c609084e4b3f5fb9806893ad68da20fd34505532
Foma Tuturov <fomiash@yandex.ru>
2022-12-24 14:55:49 +0300

    upd examples

--- hleb-project-relationship/modules/example-directory/base/templates/content.php
+++
@@ -1,4 +0,0 @@
-<?php
-/** @var  string $moduleController */
-
-echo "Demo template for controller $moduleController";
--- hleb-project-relationship/routes/example-directory/base/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::get('/standard-controller/')->controller('ExampleDirectory\TestUpdaterController');
-
-Route::get('/module-controller/')->modules('example-directory', 'Controller');
-
-
